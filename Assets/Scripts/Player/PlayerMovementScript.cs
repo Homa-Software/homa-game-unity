5a144fd05ef116f21666105a5f9ab07db7d51655
Szymon Hrabia <35381167+Survikrowa@users.noreply.github.com>
2021-12-20 00:20:43 +0100

    feat/add-proper-animation-code-for-character-movement

--- Assets/Scripts/Player/PlayerMovementScript.cs
+++ Assets/Scripts/Player/PlayerMovementScript.cs
@@ -8,9 +8,12 @@ public class PlayerMovementScript : MonoBehaviour
     public float moveSpeed = 5f;
 
     public Rigidbody2D rigidBody;
+    public Animator animator;
+
 
     private PlayerActionControls playerActionControls;
 
+    private Vector2 movement;
     float movementHorizontalInput;
     float movementVerticalInput;
 
@@ -33,14 +36,18 @@ public class PlayerMovementScript : MonoBehaviour
     {
         movementHorizontalInput = playerActionControls.Movement.MoveLeftRight.ReadValue<float>();
         movementVerticalInput = playerActionControls.Movement.MoveUpDown.ReadValue<float>();
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     void FixedUpdate()
     {
         float fixedDeltaTime = Time.fixedDeltaTime;
-        var horizontalMovement = movementHorizontalInput * moveSpeed * fixedDeltaTime;
-        var verticalMovement = movementVerticalInput * moveSpeed * fixedDeltaTime;
-        Vector2 movement = transform.position;
+        float horizontalMovement = movementHorizontalInput * moveSpeed * fixedDeltaTime;
+        float verticalMovement = movementVerticalInput * moveSpeed * fixedDeltaTime;
+
         movement.x += horizontalMovement;
         movement.y += verticalMovement;
         transform.position = movement;
